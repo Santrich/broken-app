f0767098f84dad9e507fe04d4472f9cccb2bf06b
Luis Santrich <luis.santrich@gmail.com>
2016-03-03 14:34:57 +0100

    Creating following and followers model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :posts
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :password, confirmation: true, length: { minimum: 6 }
@@ -10,4 +20,20 @@ class User < ActiveRecord::Base
   def picture
     avatar || "http://placekitten.com/64/64"
   end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
