98d59822166ba443da81fda729771c0692e70afe
Luis Santrich <luis.santrich@gmail.com>
2016-03-07 19:24:54 +0100

    Follow and unfollow pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ FacebookLite::Application.routes.draw do
     resources :posts, only: [:index, :create, :edit, :update]
   end
 
-  resources :posts, only: [:edit, :update, :destroy]
-  resources :users, only: [:edit, :update, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :posts,               only: [:edit, :update, :destroy]
+  resources :users,               only: [:edit, :update, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
 
   get '/login', to: 'sessions#new', as: :login
   get '/session', to: redirect('/login')
